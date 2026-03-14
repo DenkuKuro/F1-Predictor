f10b5e552ce5da770c32cea476a71eccd7ded18d
DenkuKuro <javier.deng17@gmail.com>
2026-03-14 11:10:33 -0700

    added few routes

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # A simple GET route
 @app.route('/')
 def home():
-    return "Hello, SFU Robot Soccer!"
+    return "F1 Prediction Game!"
 
 # A JSON API route
 @app.route('/api/status', methods=['GET'])
@@ -17,12 +17,34 @@ def get_status():
     }
     return jsonify(data), 200
 
+# A GET route to get all the predictions of a certain race
+@app.route("/api/race_predictions", methods=['GET'])
+def get_prediction_of_race():
+    race_id = request.args.get('type')
+    return 200
+
+# A GET rout to get the current race data to perform predictions
+@app.route("/api/current_race", methods=['GET'])
+def get_prediction_of_race():
+    race_id = request.args.get('type')
+    return 200
+    
 # A POST route to receive data
 @app.route('/api/predict', methods=['POST'])
 def post_prediction():
     content = request.json
     # Logic for processing prediction would go here
     return jsonify({"message": "Prediction received", "data": content}), 201
+
+
+
+@app.route("/signin", methods=['POST'])
+def signin():
+    user_info = request.json
+    # add it to data base
+    return 200
+
+
 
 if __name__ == '__main__':
     # debug=True allows for live-reloading during development
